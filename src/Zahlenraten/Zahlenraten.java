0d8b6b1b13b503274430b6b73846ba4c615103bd
Riccardo Duxenneuner <riccardo@duxi.at>
2018-11-22 15:29:07 +0100

    Zahlenraten 2

--- src/Zahlenraten/Zahlenraten.java
+++ src/Zahlenraten/Zahlenraten.java
@@ -12,12 +12,37 @@ public class Zahlenraten {
         Random rnd = new Random();
         int zufallszahl = rnd.nextInt(max - min + 1) + min;    // durch die Random Funktion wird durch zufalls Prinzip eine belibige Zahl zwischen 1 und 0 ausgewählt.
 
-        if (zufallszahl % 2 ==0)
+      int zahlen =  Integer.parseInt(JOptionPane.showInputDialog("bitte geben sie eine Zahl zwischen 1 und 10 ein!"));
+
+        if (zahlen == zufallszahl) {
+
+            JOptionPane.showMessageDialog(null, "Ihre Zahl stimmt mit der Zufallszahl überein! Glükwunsch");
+
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "Ihre Zahl stimmt mit der Zufallszahl leider nicht überein!");
+
+        }
+        if ((zufallszahl - zahlen)>=-2 && (zufallszahl - zahlen)<=2 ) {
+
+            JOptionPane.showMessageDialog(null, "Sie sind knapp an der richtigen Zahl drann!");
+        }
+
+        else
+        {
+            JOptionPane.showMessageDialog(null, "Ihre Zahl stimmt mit der Zufallszahl leider nicht überein!");
+
+        }
+        // Es schlägt fehl weil auf Auf Strings keine Operatoren angewand werden können.
+
+
+      /* if (zufallszahl % 2 ==0)
         {
             JOptionPane.showMessageDialog(null, "Die zufällig ausgewählte Zahl ist gerade!");
 
         } else {
             JOptionPane.showMessageDialog(null, "Die zufällig ausgewählte Zahl ist gerade!");
-        }
+        } */
     }
 }
